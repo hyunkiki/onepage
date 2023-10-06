678484d08f06478a82461a09e3fc7fe7acb9b53f
김현기 <rlagusrl98@gmail.com>
2023-10-06 18:16:51 +0900

    10.06 업데이트

---
+++ dist/js/comm.js
@@ -0,0 +1,10 @@
+
+new fullpage('#fullpage', {
+  //options here
+  autoScrolling: true,
+  scrollHorizontally: true,
+  navigationTooltips: ['a', 'b', 'c', 'd'],
+  anchors: ['INTRO', 'PROFILE', 'PORTFOLIO', 'CONTACT'],
+  menu: ['#navbar-nav', '#fp-nav'],
+  navigationPosition: 'le',
+});
