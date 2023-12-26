cc1fc150eac42788dc6f08cc4fb72f4e905e6de6
hyunkiki <rlagusrl98@gmail.com>
2023-12-26 22:16:20 +0900

    12-26 업데이트

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -1,9 +1,19 @@
 new fullpage("#fullpage", {
   //options here
-  autoScrolling: true,
-  scrollHorizontally: true,
+  //  autoScrolling: true,
+  //  scrollHorizontally: true,
+  //  navigationTooltips: ["a", "b", "c", "d"],
+  //  anchors: ["INTRO", "PROFILE", "PORTFOLIO", "CONTACT"],
+  //  menu: ["#navbar-nav", "#fp-nav"],
+  //  navigationPosition: "le",
+
+  navigation: true,
+  responsiveWidth: 700,
   navigationTooltips: ["a", "b", "c", "d"],
   anchors: ["INTRO", "PROFILE", "PORTFOLIO", "CONTACT"],
-  menu: ["#navbar-nav", "#fp-nav"],
-  navigationPosition: "le",
+  menu: ["#navbar-nav"],
+  parallax: true,
+  onLeave: function (origin, destination, direction) {
+    console.log("Leaving section" + origin.index);
+  },
 });
