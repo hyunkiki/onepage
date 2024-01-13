3fe7d8fcd44f53e8ad3807033c3273bcb46af1c6
hyunkiki <rlagusrl98@gmail.com>
2024-01-13 22:37:24 +0900

    01-13 업데이트

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -9,22 +9,8 @@ new fullpage("#fullpage", {
 
   navigation: true,
   responsiveWidth: 700,
-  navigationTooltips: [
-    "HOME",
-    "ABOUT",
-    "PORTFOLIO",
-    "PORTFOLIO2",
-    "PORTFOLIO3",
-    "CONTACT",
-  ],
-  anchors: [
-    "INTRO",
-    "PROFILE",
-    "PORTFOLIO",
-    "PORTFOLIO2",
-    "PORTFOLIO3",
-    "CONTACT",
-  ],
+  navigationTooltips: ["HOME", "ABOUT", "PORTFOLIO", "CONTACT"],
+  anchors: ["INTRO", "PROFILE", "PORTFOLIO", "CONTACT"],
   menu: ["#navbar-nav"],
   parallax: true,
   onLeave: function (origin, destination, direction) {
