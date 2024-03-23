fc19314a462b7b27e5a047f215e2df273d2ded7c
hyunkiki <rlagusrl98@gmail.com>
2024-03-23 23:47:10 +0900

    03-24 업데이트

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -26,23 +26,38 @@ new fullpage("#fullpage", {
   },
 });
 
-var strings = ["HYUNKI KIM'S", "PORTFOLIO"]; // 출력할 문자열을 배열로 나눕니다.
-var el = document.getElementById("str"); // HTML 문서에서 id가 'str'인 요소를 찾아서 el 변수에 할당합니다.
+let el = document.getElementsByTagName("h1")[0];
+let items = el.dataset.items.split(", ");
+TweenMax.to(el.nextElementSibling, 4, {
+  opacity: 0,
+  repeat: -1,
+  ease: Linear.easeNone,
+});
 
-function animateStrings(index) {
-  var string = strings[index];
-  var str = string.split(""); // 문자열을 한 글자씩 나누어 배열로 만듭니다.
-  var interval = setInterval(function () {
-    if (str.length > 0) {
-      el.innerHTML += str.shift(); // 배열의 첫 번째 요소를 el 요소의 innerHTML에 추가합니다.
-    } else {
-      clearInterval(interval); // 모든 문자열이 출력되면 interval을 종료합니다.
-      if (index < strings.length - 1) {
-        el.innerHTML += "<br>"; // 줄바꿈을 추가합니다.
-        animateStrings(index + 1); // 다음 문자열을 출력하기 위해 재귀적으로 함수를 호출합니다.
-      }
-    }
-  }, 90);
-}
+let tl = new TimelineMax({ repeat: -1 });
 
-animateStrings(0); // 함수 호출로 애니메이션을 시작합니다.
+let tmp = { x: 0 };
+
+items.forEach((word, idx) => {
+  let chars = word.split("");
+
+  chars.forEach((char) => {
+    tl.to(tmp, 0.1, {
+      x: "+=" + 1,
+      onComplete: () => {
+        el.textContent += char;
+      },
+    });
+  });
+
+  tl.to(tmp, 1, { x: "+=" + 1 });
+
+  chars.forEach((char) => {
+    tl.to(tmp, 0.15, {
+      x: "+=" + 1,
+      onComplete: () => {
+        el.textContent = el.textContent.slice(0, -1);
+      },
+    });
+  });
+});
