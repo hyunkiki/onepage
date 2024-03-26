1bc961ed0b52099013bd9541a5b1b2c0cfa17e8b
hyunkiki <rlagusrl98@gmail.com>
2024-03-26 21:32:55 +0900

    03-26 업데이트

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -26,38 +26,47 @@ new fullpage("#fullpage", {
   },
 });
 
-let el = document.getElementsByTagName("h1")[0];
-let items = el.dataset.items.split(", ");
-TweenMax.to(el.nextElementSibling, 4, {
-  opacity: 0,
+var cursor = $(".cursor"),
+  follower = $(".cursor_follower");
+
+var posX = 0,
+  posY = 0;
+
+var mouseX = 0,
+  mouseY = 0;
+
+TweenMax.to({}, 0.016, {
   repeat: -1,
-  ease: Linear.easeNone,
+  onRepeat: function () {
+    posX += (mouseX - posX) / 9;
+    posY += (mouseY - posY) / 9;
+
+    TweenMax.set(follower, {
+      css: {
+        left: posX - 12,
+        top: posY - 12,
+      },
+    });
+    TweenMax.set(cursor, {
+      css: {
+        left: mouseX,
+        top: mouseY,
+      },
+    });
+  },
 });
 
-let tl = new TimelineMax({ repeat: -1 });
+$(document).on("mousemove", function (e) {
+  mouseX = e.pageX;
+  mouseY = e.pageY;
+});
 
-let tmp = { x: 0 };
+$(".link").on("mouseenter", function () {
+  cursor.addClass("active");
+  follower.addClass("active");
+});
 
-items.forEach((word, idx) => {
-  let chars = word.split("");
-
-  chars.forEach((char) => {
-    tl.to(tmp, 0.1, {
-      x: "+=" + 1,
-      onComplete: () => {
-        el.textContent += char;
-      },
-    });
-  });
-
-  tl.to(tmp, 1, { x: "+=" + 1 });
-
-  chars.forEach((char) => {
-    tl.to(tmp, 0.15, {
-      x: "+=" + 1,
-      onComplete: () => {
-        el.textContent = el.textContent.slice(0, -1);
-      },
-    });
-  });
+$(".link").on("mouseleave", function () {
+  cursor.removeClass("active");
+  follower.removeClass("active");
 });
